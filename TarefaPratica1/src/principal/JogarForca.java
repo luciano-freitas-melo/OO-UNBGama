86018812f96aa6e79632216ab0c0d1ee94b427ee
luciano-freitas-melo <lucianodefreitasmelo@hotmail.com>
2022-01-25 13:18:58 -0300

    Menu do jogo
    - JogodaForca
    -- implementado o menu com as opções do programa para o usuário
    
    - JogarForca
    -- Mudada para cá a opção de jogar novamente a forca.

--- TarefaPratica1/src/principal/JogarForca.java
+++ TarefaPratica1/src/principal/JogarForca.java
@@ -8,9 +8,43 @@ public class JogarForca {
 	private static int indexTema; // Essa variável é o índice do tema que será jogado nessa partida.
 	private static String palavraSecreta; // Guarda a palavra secreta que será usada durante o jogo.
 	
-	
-	
 	public static void jogar(String[][] temas) {
+		String jogarNovamente; // Recebe S ou N, dependendo se o usuário deseja jogar novamente ou não.
+		
+		do {
+			JogarForca.jogarRodada(temas);
+			jogarNovamente = validarJogarNovamente();
+			
+		}while (jogarNovamente.equals("S") ? true : false);
+
+	}
+	
+	private static String validarJogarNovamente() {
+		String input;
+		
+		do {
+			input = Utensilios.ler.next().toUpperCase();
+			
+		} while (!JogarNovamenteValido(input));
+		
+		return input;
+	}
+
+	private static boolean JogarNovamenteValido(String input) {
+		boolean inputValido = true;
+		
+		if(!Utensilios.letraValida(input))
+			inputValido = false;
+		
+		else if (!input.equals("S") && !input.equals("N")) {
+			inputValido = false;
+			System.out.print("Só é permitido digitar S (para SIM) ou N (para NÃO): ");
+		}
+	
+		return inputValido;
+	}
+
+	private static void jogarRodada(String[][] temas) {
 		
 		
 		imprimeMensagemAbertura();
